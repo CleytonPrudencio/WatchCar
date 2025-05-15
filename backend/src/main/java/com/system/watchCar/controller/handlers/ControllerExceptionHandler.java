d1ece93fb3b132c161dce2f51ab6421f426ba591
ElderBR <elderbarbosaromao@gmail.com>
2025-05-15 20:04:50 -0300

    Criado classe auxiliar para dá a resposter quando houver erro

--- backend/src/main/java/com/system/watchCar/controller/handlers/ControllerExceptionHandler.java
+++ backend/src/main/java/com/system/watchCar/controller/handlers/ControllerExceptionHandler.java
@@ -1,4 +1,34 @@
 package com.system.watchCar.controller.handlers;
 
+import com.system.watchCar.dto.exceptions.ErrorDTO;
+import com.system.watchCar.dto.exceptions.ErrorResponseDTO;
+import com.system.watchCar.dto.exceptions.ErrorsDTO;
+import com.system.watchCar.service.exceptions.UserExecption;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
 public class ControllerExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> methodArgumentNotValidException(MethodArgumentNotValidException error, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        return ResponseEntity.status(status).body(new ErrorsDTO(status, error, request));
+    }
+
+    @ExceptionHandler(UserExecption.class)
+    public ResponseEntity<ErrorDTO> userException(UserExecption error, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        return ResponseEntity.status(status).body(new ErrorResponseDTO(status, error, request).response());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorDTO> illegalArgumentException(IllegalArgumentException error, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return ResponseEntity.status(status).body(new ErrorDTO(status, error, request));
+    }
 }
