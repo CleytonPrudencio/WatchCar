048d4e55ee467b3c1ab82a4b72c8b671ab5c4e53
ElderBR <elderbarbosaromao@gmail.com>
2025-05-24 11:29:46 -0300

    BackEnd: Criado as classes de Respository, Service e Controller para o Tipo Ocorrência

--- backend/src/main/java/com/system/watchCar/controller/TipoOcorrenciaController.java
+++ backend/src/main/java/com/system/watchCar/controller/TipoOcorrenciaController.java
@@ -1,4 +1,25 @@
 package com.system.watchCar.controller;
 
+import com.system.watchCar.dto.TipoOcorrenciaDTO;
+import com.system.watchCar.service.TipoOcorrenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tipos_ocorrencias")
 public class TipoOcorrenciaController {
+
+    @Autowired
+    private TipoOcorrenciaService tipoOcorrenciaService;
+
+    @GetMapping
+    public ResponseEntity<List<TipoOcorrenciaDTO>> findAll() {
+        return ResponseEntity.ok(tipoOcorrenciaService.findAll());
+    }
+
 }
--- backend/src/main/java/com/system/watchCar/repository/TipoOcorrenciaRepository.java
+++ backend/src/main/java/com/system/watchCar/repository/TipoOcorrenciaRepository.java
@@ -1,4 +1,9 @@
 package com.system.watchCar.repository;
 
-public interface TipoOcorrenciaRepository {
+import com.system.watchCar.entity.TipoOcorrencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoOcorrenciaRepository extends JpaRepository<TipoOcorrencia, Long> {
 }
