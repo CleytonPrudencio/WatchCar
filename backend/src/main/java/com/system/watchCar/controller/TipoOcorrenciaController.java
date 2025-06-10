063f00145466c7c6507c18866414427845877455
ElderBR <elderbarbosaromao@gmail.com>
2025-06-10 19:51:33 -0300

    BackEnd: Alterado o endpoint do Tipo de ocorrencia

--- backend/src/main/java/com/system/watchCar/controller/TipoOcorrenciaController.java
+++ backend/src/main/java/com/system/watchCar/controller/TipoOcorrenciaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/tipos_ocorrencias")
+@RequestMapping("/api/tipos_ocorrencias")
 public class TipoOcorrenciaController {
 
     @Autowired
